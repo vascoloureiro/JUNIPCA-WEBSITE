cb4a2e842af576d5a2256a3cd9f17350332306a9
vascoloureiro <vascoloureiro2004@gmail.com>
2025-10-01 16:30:54 +0100

    Correction of build error

--- src/LandPage/landpage_Index.jsx
+++ src/LandPage/landpage_Index.jsx
@@ -3,9 +3,6 @@ import HeaderLandPage from './Header/Header.tsx';
 import Footer from './Footer/Footer';
 import HeroSection from './Hero/Hero.tsx';
 import Features from './Features/FeaturesSection.tsx';
-import PricingSection from './Pricing/PricingSection.tsx';
-import TestimonialsSection from './Testimonial.tsx/TestimonialsSection.tsx';
-import SideBarMenu from '../SideBarMenu/sideBarMenu.jsx';
 import WhoAreWe from './WhoAreWe/WhoAreWe.tsx';
 import Team from './Team/team.tsx';
 
